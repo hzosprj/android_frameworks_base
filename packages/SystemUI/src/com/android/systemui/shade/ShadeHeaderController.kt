b32691cc5fff56dbab0fbda29cc4a9f5b0095218
ReallySnow <reallysnow233@gmail.com>
2022-09-15 13:38:48 +0800

    SystemUI: Follow light/dark theme in SplitShade Header
    * Google's default implementation is dark, which means
      it doesn't need to follow the light/dark color change,
      but we broke it, so add it to complete the SplitShade
      Header color change
    
    neobuddy89: Rewrite with some fixes.
    
    Change-Id: I5464039885197eeb43bd31b822bfcba7a1b08776
    Co-authored-by: Col_or <col_or@qq.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -23,7 +23,9 @@ import android.app.PendingIntent
 import android.app.StatusBarManager
 import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.content.res.Configuration
+import android.graphics.Color
 import android.graphics.Insets
 import android.os.Bundle
 import android.os.Trace
@@ -337,6 +339,11 @@ constructor(
                 date.setTextAppearance(R.style.TextAppearance_QS_Status)
                 mShadeCarrierGroup.updateTextAppearance(R.style.TextAppearance_QS_Status)
             }
+
+            override fun onUiModeChanged() {
+                updateResources()
+            }
+
         }
 
     private val nextAlarmCallback =
@@ -420,6 +427,7 @@ constructor(
         demoModeController.addCallback(demoModeReceiver)
         statusBarIconController.addIconGroup(iconManager)
         nextAlarmController.addCallback(nextAlarmCallback)
+        updateResources()
         systemIconsHoverContainer.setOnHoverListener(
             statusOverlayHoverListenerFactory.createListener(systemIconsHoverContainer)
         )
@@ -632,6 +640,19 @@ constructor(
         header.setPadding(padding, header.paddingTop, padding, header.paddingBottom)
         updateQQSPaddings()
         qsBatteryModeController.updateResources()
+
+        val textColorPrimary = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        val colorStateList = Utils.getColorAttr(context, android.R.attr.textColorPrimary)
+        val textColorSecondary = Utils.getColorAttrDefaultColor(context,
+                android.R.attr.textColorSecondary)
+        iconManager.setTint(
+            textColorPrimary,
+            Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimaryInverse),
+        )
+        clock.setTextColor(textColorPrimary)
+        date.setTextColor(textColorPrimary)
+        mShadeCarrierGroup.updateColors(textColorPrimary, colorStateList)
+        batteryIcon.updateColors(textColorPrimary, textColorSecondary, textColorPrimary)
     }
 
     private fun updateQQSPaddings() {
