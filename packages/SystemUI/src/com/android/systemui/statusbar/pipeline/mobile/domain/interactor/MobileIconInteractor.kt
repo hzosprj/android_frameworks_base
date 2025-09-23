311d221db9bbd8a02bcd86989c6204e451c17ef4
HDzungx <hdzungx@gmail.com>
2025-09-24 05:47:10 +0700

    Revert "SystemUI: Add roaming indicator to statusbar tuner"
    This reverts commit 9f4ae6cb65528c596677155448b84b1170c8e560.

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconInteractor.kt
@@ -123,9 +123,6 @@ interface MobileIconInteractor {
     /** See [MobileIconsInteractor.isRoamingForceHidden]. */
     val isRoamingForceHidden: Flow<Boolean>
 
-    /** See [MobileIconsInteractor.isRoamingForceHidden]. */
-    val isRoamingForceHidden: Flow<Boolean>
-
     /** See [MobileIconsInteractor.isForceHidden]. */
     val isForceHidden: Flow<Boolean>
 
