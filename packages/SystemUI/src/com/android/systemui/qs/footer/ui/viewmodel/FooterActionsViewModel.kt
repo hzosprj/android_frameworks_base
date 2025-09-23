1bd830cc4c2163d14a1bfde71af03ef81484ef7d
HDzungx <hdzungx@gmail.com>
2025-09-24 05:46:09 +0700

    Revert "SystemUI: Launch power menu from QS only if enabled"
    This reverts commit 516c6aec977a87c5db978dfa683371963b0a86c6.

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -17,8 +17,6 @@
 package com.android.systemui.qs.footer.ui.viewmodel
 
 import android.content.Context
-import android.os.UserHandle
-import android.provider.Settings
 import android.util.Log
 import android.view.ContextThemeWrapper
 import androidx.lifecycle.DefaultLifecycleObserver
@@ -41,7 +39,6 @@ import com.android.systemui.res.R
 import com.android.systemui.shade.ShadeDisplayAware
 import com.android.systemui.shade.domain.interactor.ShadeModeInteractor
 import com.android.systemui.shade.shared.model.ShadeMode
-import com.android.systemui.statusbar.policy.KeyguardStateController
 import com.android.systemui.util.icuMessageFormat
 import javax.inject.Inject
 import javax.inject.Named
@@ -124,7 +121,6 @@ class FooterActionsViewModel(
         private val globalActionsDialogLiteProvider: Provider<GlobalActionsDialogLite>,
         private val activityStarter: ActivityStarter,
         @Named(PM_LITE_ENABLED) private val showPowerButton: Boolean,
-        private val keyguardStateController: KeyguardStateController
     ) {
         /** Create a [FooterActionsViewModel] bound to the lifecycle of [lifecycleOwner]. */
         fun create(lifecycleOwner: LifecycleOwner): FooterActionsViewModel {
@@ -152,7 +148,6 @@ class FooterActionsViewModel(
                 globalActionsDialogLite,
                 activityStarter,
                 showPowerButton,
-                keyguardStateController,
             )
         }
 
@@ -178,7 +173,6 @@ class FooterActionsViewModel(
                 globalActionsDialogLite,
                 activityStarter,
                 showPowerButton,
-                keyguardStateController,
             )
         }
     }
@@ -192,7 +186,6 @@ fun createFooterActionsViewModel(
     globalActionsDialogLite: GlobalActionsDialogLite,
     activityStarter: ActivityStarter,
     showPowerButton: Boolean,
-    keyguardStateController: KeyguardStateController
 ): FooterActionsViewModel {
     suspend fun observeDeviceMonitoringDialogRequests(quickSettingsContext: Context) {
         footerActionsInteractor.deviceMonitoringDialogRequests.collect {
@@ -243,11 +236,6 @@ fun createFooterActionsViewModel(
     }
 
     fun onPowerButtonClicked(expandable: Expandable) {
-        if (keyguardStateController.isShowing() && keyguardStateController.isMethodSecure() 
-                && Settings.System.getIntForUser(appContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1, UserHandle.USER_CURRENT) == 0) {
-            return
-        }
         if (falsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
             return
         }
